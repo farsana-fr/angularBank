8705971725dbf74ea0d6f1a01b7ccb05f8994cda
unknown <farsanarahimfr@gmail.com>
2023-03-21 18:27:01 +0530

    Deposit n Withdraw

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,10 +4,10 @@ import { DataService } from '../services/data.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-  user:any;
+  user: any;
 
   inputAccNo = 'Enter Account No';
   inputPWD = 'Enter your Password';
@@ -15,19 +15,43 @@ export class DashboardComponent {
 
   accNo = '';
   pwd: any;
+  amt = '';
 
-  constructor(private ds:DataService)
-  {
-    this.user=this.ds.currentUser;
+  accNo1 = '';
+  pwd1: any;
+  amt1 = '';
+
+  constructor(private ds: DataService) {
+    this.user = this.ds.currentUser;
   }
-  ngOnInit():void{}
+  ngOnInit(): void {}
 
-  deposit()
-  {
-
+  deposit() {
+    var accNo = this.accNo;
+    var pwd = this.pwd;
+    var amt = Number.parseInt(this.amt);
+    const newBalnc = this.ds.deposit(accNo, pwd, amt);
+    console.log("Deposit");
+    if (newBalnc) {
+      this.ds.userDetails[accNo]['balance'] += amt;
+      alert(`Your account is credited with Rs.${amt}.\nBalance is Rs.${newBalnc}`);
+    }
   }
-  withdraw()
-  {
-    
+  withdraw() {
+    var accNo = this.accNo1;
+    var pwd = this.pwd1;
+    var amt = Number.parseInt(this.amt1);
+    var currentBalance=this.ds.userDetails[accNo]["balance"];
+    console.log("Withdraw");
+    if(currentBalance>0 && currentBalance>=amt)
+    {
+      const newBalnc=this.ds.withdraw(accNo,pwd,amt);
+     console.log(this.ds.userDetails[accNo]["balance"]);
+     alert(`Your account is debited with Rs.${amt}.\nBalance is Rs.${newBalnc}`);
+    }
+    else
+    {
+      alert(`Insufficient Balance!!`);
+    }
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-  currentUser:any;
-  constructor() { }
+  currentUser: any;
+  constructor() {}
 
   userDetails: any = {
     1001: { acno: 1001, username: 'amal', password: 'abc123', balance: 0 },
@@ -14,31 +14,58 @@ export class DataService {
     1004: { acno: 1004, username: 'anu', password: 'abc123', balance: 0 },
   };
 
-  register(uName:any,acno:any,pwd:any)
-  {
-    if(acno in this.userDetails)
-    {
+  register(uName: any, acno: any, pwd: any) {
+    if (acno in this.userDetails) {
       return false;
-    }
-    else
-    {
-      this.userDetails[acno]={acno,username:uName,password:pwd,balance:0}
+    } else {
+      this.userDetails[acno] = {
+        acno,
+        username: uName,
+        password: pwd,
+        balance: 0,
+      };
       return true;
     }
   }
-  login(acno:any,pwd:any)
-  {
-    var uDetails=this.userDetails;
+  login(acno: any, pwd: any) {
+    var uDetails = this.userDetails;
     if (acno in uDetails) {
       console.log(uDetails[acno]['password']);
       if (pwd == uDetails[acno]['password']) {
-        this.currentUser=uDetails[acno]["username"];
+        this.currentUser = uDetails[acno]['username'];
         return true;
       } else {
         return false;
       }
     } else {
       return false;
-    } 
+    }
+  }
+
+  deposit(acno: any, pwd: any, amt: any) {
+    if (acno in this.userDetails) {
+      if (pwd == this.userDetails[acno]['password']) {
+        this.userDetails[acno]['balance'] += amt;
+        return this.userDetails[acno]['balance'];
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  withdraw(acno:any,pwd: any, amt: any)
+  {
+    if (acno in this.userDetails) {
+      if (pwd == this.userDetails[acno]['password']) {
+        this.userDetails[acno]['balance'] -= amt;
+        return this.userDetails[acno]['balance'];
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
 }
